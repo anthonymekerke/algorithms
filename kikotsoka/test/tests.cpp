c2386e1fd628fb171363e64efbe5e59f2ee18acb
Mekerke_Anthony <anthony.mekerker@gmail.com>
2022-05-03 22:22:48 +0200

    user storie 18: test written.

--- kikotsoka/test/tests.cpp
+++ kikotsoka/test/tests.cpp
@@ -12,7 +12,7 @@
 
 using namespace kikotsoka;
 
-TEST_CASE("to_string", "coordinates")
+TEST_CASE("storie 1", "to_string")
 {
     const Coordinates coordinates(0, 0);
     const std::string str = coordinates.to_string();
@@ -20,7 +20,7 @@ TEST_CASE("to_string", "coordinates")
     REQUIRE(str == "A1");
 }
 
-TEST_CASE("has column and line", "coordinates")
+TEST_CASE("storie 1 bis", "has column and line")
 {
     const Coordinates coordinates(2, 4);
 
@@ -28,7 +28,7 @@ TEST_CASE("has column and line", "coordinates")
     REQUIRE(coordinates.line() == 5);
 }
 
-TEST_CASE("coordinates equality", "coordinates")
+TEST_CASE("storie 2", "coordinates equality")
 {
     const Coordinates c1(2, 1);
     const Coordinates c2(2, 1);
@@ -41,7 +41,7 @@ TEST_CASE("coordinates equality", "coordinates")
     REQUIRE_FALSE(c1 == c4);
 }
 
-TEST_CASE("has 144 empty cells", "engine")
+TEST_CASE("storie 3", "has 144 empty cells")
 {
     const Engine engine;
 
@@ -53,14 +53,14 @@ TEST_CASE("has 144 empty cells", "engine")
     }
 }
 
-TEST_CASE("black starts", "game")
+TEST_CASE("storie 4", "black starts")
 {
     const Engine engine;
 
     REQUIRE(engine.current_color() == Color::BLACK);
 }
 
-TEST_CASE("start pawn number", "game")
+TEST_CASE("storie 5", "start pawn number")
 {
     const Engine engine;
 
@@ -68,7 +68,7 @@ TEST_CASE("start pawn number", "game")
     REQUIRE(engine.white_pawn_number() == 60);
 }
 
-TEST_CASE("black first move","game")
+TEST_CASE("storie 6", "black first move")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -83,7 +83,7 @@ TEST_CASE("black first move","game")
     REQUIRE(engine.state(h6) == State::BLACK);
 }
 
-TEST_CASE("black 59 pawn left", "game")
+TEST_CASE("storie 7", "black 59 pawn left")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -94,7 +94,7 @@ TEST_CASE("black 59 pawn left", "game")
     REQUIRE(engine.black_pawn_number() == 59);
 }
 
-TEST_CASE("white has to play", "game")
+TEST_CASE("storie 8", "white has to play")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -105,7 +105,7 @@ TEST_CASE("white has to play", "game")
     REQUIRE(engine.current_color() == Color::WHITE);
 }
 
-TEST_CASE("white first move", "game")
+TEST_CASE("storie 9", "white first move")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -124,9 +124,13 @@ TEST_CASE("white first move", "game")
     REQUIRE(engine.white_pawn_number() == 59);
 }
 
-TEST_CASE("cells blocked after pattern", "game")
+TEST_CASE("storie 10", "cells blocked after pattern")
 {
     Engine engine;
+
+    /*
+    Moves: N-H6, B-I7, N-G5, B-J8, N-F4
+    */
     Coordinates h6 = Coordinates(7,5);
     Coordinates i7 = Coordinates(8,6);
     Coordinates g5 = Coordinates(6,4);
@@ -156,15 +160,15 @@ TEST_CASE("cells blocked after pattern", "game")
     REQUIRE(engine.state(g5) == State::BLACK);
     REQUIRE(engine.state(h6) == State::BLACK);
 
-    REQUIRE(engine.state(f5) == State::BLOCK);
-    REQUIRE(engine.state(f6) == State::BLOCK);
-    REQUIRE(engine.state(g4) == State::BLOCK);
-    REQUIRE(engine.state(g6) == State::BLOCK);
-    REQUIRE(engine.state(h4) == State::BLOCK);
-    REQUIRE(engine.state(h5) == State::BLOCK);
+    REQUIRE(engine.state(f5) == State::LOCK);
+    REQUIRE(engine.state(f6) == State::LOCK);
+    REQUIRE(engine.state(g4) == State::LOCK);
+    REQUIRE(engine.state(g6) == State::LOCK);
+    REQUIRE(engine.state(h4) == State::LOCK);
+    REQUIRE(engine.state(h5) == State::LOCK);
 }
 
-TEST_CASE("black goes level 1", "game")
+TEST_CASE("storie 11", "black goes level 1")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -189,7 +193,7 @@ TEST_CASE("black goes level 1", "game")
     REQUIRE(engine.black_level() == 1);
 }
 
-TEST_CASE("black takes 2 pawns from white", "game")
+TEST_CASE("storie 12", "black takes 2 pawns from white")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -213,7 +217,7 @@ TEST_CASE("black takes 2 pawns from white", "game")
     REQUIRE(engine.white_pawn_number() == 58);
 }
 
-TEST_CASE("cells blocked after pattern, white pawns taken", "game")
+TEST_CASE("storie 13", "cells blocked after pattern, white pawns taken")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -242,15 +246,15 @@ TEST_CASE("cells blocked after pattern, white pawns taken", "game")
     REQUIRE(engine.state(g5) == State::BLACK);
     REQUIRE(engine.state(h6) == State::BLACK);
 
-    REQUIRE(engine.state(h5) == State::BLOCK);
-    REQUIRE(engine.state(g4) == State::BLOCK);
-    REQUIRE(engine.state(h4) == State::BLOCK);
-    REQUIRE(engine.state(g6) == State::BLOCK);
-    REQUIRE(engine.state(f5) == State::BLOCK);
-    REQUIRE(engine.state(f6) == State::BLOCK);
+    REQUIRE(engine.state(h5) == State::LOCK);
+    REQUIRE(engine.state(g4) == State::LOCK);
+    REQUIRE(engine.state(h4) == State::LOCK);
+    REQUIRE(engine.state(g6) == State::LOCK);
+    REQUIRE(engine.state(f5) == State::LOCK);
+    REQUIRE(engine.state(f6) == State::LOCK);
 }
 
-TEST_CASE("Black get 2 points", "game")
+TEST_CASE("storie 14", "Black get 2 points")
 {
     Engine engine;
     Coordinates h6 = Coordinates(7,5);
@@ -273,7 +277,7 @@ TEST_CASE("Black get 2 points", "game")
     REQUIRE(engine.black_score() == 2);
 }
 
-TEST_CASE("White makes first pattern", "game")
+TEST_CASE("storie 15", "White makes first pattern")
 {
     Engine engine;
 
@@ -309,12 +313,12 @@ TEST_CASE("White makes first pattern", "game")
     REQUIRE(engine.state(j8) == State::WHITE);
     REQUIRE(engine.state(k9) == State::WHITE);
 
-    REQUIRE(engine.state(i8) == State::BLOCK);
-    REQUIRE(engine.state(i9) == State::BLOCK);
-    REQUIRE(engine.state(j7) == State::BLOCK);
-    REQUIRE(engine.state(j9) == State::BLOCK);
-    REQUIRE(engine.state(k7) == State::BLOCK);
-    REQUIRE(engine.state(k8) == State::BLOCK);
+    REQUIRE(engine.state(i8) == State::LOCK);
+    REQUIRE(engine.state(i9) == State::LOCK);
+    REQUIRE(engine.state(j7) == State::LOCK);
+    REQUIRE(engine.state(j9) == State::LOCK);
+    REQUIRE(engine.state(k7) == State::LOCK);
+    REQUIRE(engine.state(k8) == State::LOCK);
 
     REQUIRE(engine.white_pawn_number() == 57);
     REQUIRE(engine.white_level() == 1);
@@ -322,7 +326,7 @@ TEST_CASE("White makes first pattern", "game")
 }
 
 
-TEST_CASE("black form second pattern", "game")
+TEST_CASE("storie 16", "black form second pattern")
 {
     Engine engine;
     Coordinates coords;
@@ -346,7 +350,7 @@ TEST_CASE("black form second pattern", "game")
     REQUIRE(engine.black_pawn_number() == 55);
 }
 
-TEST_CASE("White last move blocks black", "game")
+TEST_CASE("storie 17", "White last move blocks black")
 {
     Engine engine;
     Coordinates coords;
@@ -360,18 +364,48 @@ TEST_CASE("White last move blocks black", "game")
     for(size_t i = 0; i < columns.size(); ++i){
         coords = Coordinates(columns[i], lines[i]);
 
+        engine.block_cells();
         engine.move(coords);
         engine.effect(coords);
         engine.switch_player();
     }
+
+    engine.block_cells();
 
     Coordinates b3 = Coordinates(1, 2);
     Coordinates a4 = Coordinates(0, 3);
     Coordinates c4 = Coordinates(2, 3);
     Coordinates b5 = Coordinates(1, 4);
 
-    REQUIRE(engine.state(b3) == State::BLOCK_TMP);
-    REQUIRE(engine.state(a4) == State::BLOCK_TMP);
-    REQUIRE(engine.state(c4) == State::BLOCK_TMP);
-    REQUIRE(engine.state(b5) == State::BLOCK_TMP);
+    REQUIRE(engine.state(b3) == State::BLOCK);
+    REQUIRE(engine.state(a4) == State::BLOCK);
+    REQUIRE(engine.state(c4) == State::BLOCK);
+    REQUIRE(engine.state(b5) == State::BLOCK);
+}
+
+TEST_CASE("storie 18", "can't form lower pattern")
+{
+    Engine engine;
+    Coordinates coords;
+
+    /*
+    Moves: N-H6, B-I7, N-G5, B-J8, N-F4, B-K9, N-H7, B-J4, N-G8, B-J5, try: N-F9
+    */
+    std::vector<int> columns = {7,8,6,9,5,10,7,9,6,9};
+    std::vector<int> lines   = {5,6,4,7,3,8,6,3,7,4};
+
+    for(size_t i = 0; i < columns.size(); ++i){
+        coords = Coordinates(columns[i], lines[i]);
+
+        engine.block_cells();
+        engine.move(coords);
+        engine.effect(coords);
+        engine.switch_player();
+    }
+
+    engine.block_cells();
+    
+    coords = Coordinates(5,8); //F9
+
+    REQUIRE(engine.state(coords) == State::BLOCK);
 }
